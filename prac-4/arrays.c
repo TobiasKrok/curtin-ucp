cd7c86d8cd1a038e64f57049d80b8e4f56868c97
TobiasKrok <ekoncraft@hotmail.no>
2023-03-28 17:59:08 +0800

    Prac 4

--- prac-4/arrays.c
+++ prac-4/arrays.c
@@ -1,6 +1,52 @@
-#include <stdio.io>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "arrays.h"
 
 int main() {
 
-    return 
+    int arr[] = {10, 20, 30};
+    int *arr2 = (int*)malloc(sizeof(int) * 3);
+    fill_values(arr2, 3, 5);
+    fill_values(arr, 3, 104545);
+
+    printf("%d \n", sum(arr2, 3));
+    printf("%d \n", sum(arr, 3));
+
+    return 0;
+}
+
+int sum(int *array, int size) {
+
+    int i;
+    int res;
+    res = 0;
+    for(i = 0; i < size; i++ ) {
+        printf("%d \n", array[i]);
+        res += array[i];
+    }
+
+    return res;
+}
+
+int max(int *array, int size) {
+
+    int res;
+    res = array[0];
+    int i;
+    for(i = 1; i < size; i++) {
+        if(array[i] > res) {
+            res = array[]
+        }
+    } 
+    return 0;
+}
+
+
+void fill_values(int* arr, int size, int value) {
+
+    int i;
+    for(i = 0; i < size; i++) {
+        arr[i] = value;
+    }
 }
---
+++ prac-4/arrays.h
@@ -0,0 +1,8 @@
+#ifndef ARRAYS_H
+#define ARRAYS_H
+
+int sum(int *array, int size);
+int max(int *array, int size);
+void fill_values(int* arr, int size, int value);
+
+#endif
